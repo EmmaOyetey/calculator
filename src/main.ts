07f8066486b65fe30eb02395ddae8f3bc6989dfe
Emma Oyetey <emmaflookis@hotmail.com>
2024-03-10 10:47:31 +0000

    added loop to handle instance user hits equals

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import "./style.scss";
 
 const button = document.querySelectorAll<HTMLButtonElement>(".button");
 const digit = document.querySelectorAll<HTMLButtonElement>(".digit");
-const operation  = document.querySelectorAll<HTMLButtonElement>(".operation");
+const convert  = document.querySelectorAll<HTMLButtonElement>(".convert");
+const equal = document.querySelectorAll<HTMLButtonElement>(".equals");
 
 //Add Query Selectors and return type HTML 
 //to know what button has been clicked - Get and return individual digit elements
@@ -28,10 +29,10 @@ const DivButton = document.querySelector<HTMLButtonElement>(".operation__div");
 const addButton = document.querySelector<HTMLButtonElement>(".operation__add");
 const subButton = document.querySelector<HTMLButtonElement>(".operation__sub");
 
-const equalsButton = document.querySelector(".conver__posNeg");
-const posToNegButton = document.querySelector(".convert__dec");
-const decButton = document.querySelector(".convert__percent");
-const percentButton = document.querySelector(".operation__mult");
+const equalsButton = document.querySelector(".equals");
+const posToNegButton = document.querySelector(".convert__posNeg");
+const decButton = document.querySelector(".convert__dec");
+const percentButton = document.querySelector(".convert__percent");
 const clearButton = document.querySelector(".clear");
 
 
@@ -44,6 +45,14 @@ if (!button) {
 if (!digit) {
     throw new Error ("issue with digit button");
     }
+
+if (!convert) {
+    throw new Error ("issue with covert button")
+}
+
+if (!equal){
+    throw new Error ("issue with covert button")
+}
 
 if (!digitOneButton) {
     throw new Error ("issue with digit1");     // do we need to be this specific? and add for each individual button?
@@ -214,7 +223,32 @@ const divide = (event: Event): void => {
             console.log(numberInCalculation, operatorsInCalculation, enteredDigit);
             
             };
-    
+
+        const equals = (event: Event): void => {
+            let concatenatedNumber: number = 0; 
+            for (let index = 0; index < enteredDigit.length; index++) {
+                   concatenatedNumber += enteredDigit[index] * Math.pow(10, enteredDigit.length - index - 1);
+               }
+            numberInCalculation.push(concatenatedNumber);
+
+            enteredDigit=[]; 
+
+            let answer: number = numberInCalculation[0];
+                for (let index = 1; index < numberInCalculation.length; index++) {
+                    if (operatorsInCalculation[index - 1] === "+") {
+                        answer += numberInCalculation[index];
+                    } else if (operatorsInCalculation[index - 1] === "-") {
+                        answer -= numberInCalculation[index];
+                    } else if (operatorsInCalculation[index - 1] === "/") {
+                        answer /= numberInCalculation[index];
+                    } else if (operatorsInCalculation[index - 1] === "x") {
+                        answer *= numberInCalculation[index];
+                    } else {
+                        console.log("Invalid operator:", operatorsInCalculation[index - 1]);
+                    }
+                }
+                console.log (numberInCalculation, operatorsInCalculation, enteredDigit, answer);
+            };
 
     //    const multiply =(event: Event):void => {
     //       let answer:number = numberInCalculation [0];
@@ -227,7 +261,9 @@ const divide = (event: Event): void => {
     //       let answer:number = numberInCalculation [0];
     //        for (let index = 0; index < enteredDigit.length; index++) {
     //           if ( operatorsInCalculation = "+" {answer += answer + numberInCalculation[index];
-    //        }
+    //        } else if (operatorsInCalculation = "-") {answer += answer - numberInCalculation[index];
+//              } else if (operatorsInCalculation = "/") { answer += answer / numberInCalculation[index]};
+//                      else if (operatorsInCalculation = "x") { answer += answer x numberInCalculation[index]};
     //    }
 
 if (multButton) {
@@ -247,11 +283,11 @@ if(subButton) {
         subButton.addEventListener("click", subtract);
 };
 
-//if(equalsButton) {
-//    equalsButton.addEventListener("click" , equals); 
-//}
+if(equalsButton) {
+  equalsButton.addEventListener("click" , equals); 
+}
 
-//To avoid repetition of the concatenation need to incorporate;
+//To avoid repetition of the concatenation could incorporate seperate function for concatenate;
 //const concatenateNumber = (enteredDigit: number[]): number => {
 //    let concatenatedNumber: number = 0; // Initialize concatenatedNumber variable before the loop
 //    for (let index = 0; index < enteredDigit.length; index++) {
